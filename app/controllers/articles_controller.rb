d2bbf73735c0a1dac0fa74fceaf3af6185cd0e6b
Kishawn Meeranazeer <Kishawn.meera@hotmail.com>
2018-03-12 03:39:03 -0400

    Added the list and navigation

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,11 +1,18 @@
 class ArticlesController < ApplicationController
+  def index
+    @articles = Article.all
+  end
   def new
     @article = Article.new
   end
 
+  def edit
+    @article = Article.find(params[:id])
+
+  end
+
   def create
       @article = Article.new(article_param)
-
       if @article.save
         flash[:notice] = "Article was successfully created"
         redirect_to article_path(@article)
@@ -13,12 +20,19 @@ class ArticlesController < ApplicationController
         render 'new'
       end
   end
+
+  def update
+    @article = Article.find(params[:id])
+    if @article.update(article_param)
+      flash[:notice] = "Article was successfully updated"
+      redirect_to article_path(@article)
+    else
+      render 'edit'
+    end
+
+  end
   def show
     @article = Article.find(params[:id])
-
-
-
-
   end
     private
       def article_param
